863389344096b37820d6ca250e4adca675883806
Thanos <61045882+KokkinosThanos@users.noreply.github.com>
2021-05-25 20:07:26 +0300

    Add files via upload

--- client/app.js
+++ client/app.js
@@ -1,4 +1,3 @@
-
 const GRAPHQL_URL = "https://solar-system.kokkinos-thanos.com/graphql";
 
 async function fetchResults() {
@@ -29,13 +28,9 @@ async function fetchResults() {
 }
 
 fetchResults().then(({ planets }) => {
-
   const dataDom = document.querySelector(".data");
 
   for (var i = 0; i < planets.length; i++) {
-    console.log(Object.values(planets[i]));
-    console.log("Length: " + i);
-
     dataDom.innerHTML += `<ul class="ul${i}"> `;
 
     // ---  --- --- --- ---
@@ -44,7 +39,6 @@ fetchResults().then(({ planets }) => {
 
     Object.entries(planets[i]).forEach((entry) => {
       const [key, value] = entry;
-      console.log(`${key}: ${value}`);
 
       ulCreate.innerHTML += `<li>  ${value} </li>`;
     });
@@ -53,34 +47,27 @@ fetchResults().then(({ planets }) => {
   let ul = document.querySelectorAll("ul");
 
   for (var j = 0; j < ul.length; j++) {
-
     let node = ul[j].childNodes;
 
     let planet = node[1].textContent;
-    console.log("value...", planet);
     node[1].innerHTML = `<h2 class="planet"> ${planet} </h2>`;
 
     let description = node[2].textContent;
-    console.log(description);
     node[2].innerHTML = `<p class="description"> ${description} </p>`;
 
     let tempeture = node[3].textContent;
-    console.log(tempeture);
     node[3].innerHTML = `<li class="key"> Average Tempeture </li> <li class="value">${tempeture} C </li> `;
 
     let rotation = node[4].textContent;
-    console.log(rotation);
     node[4].innerHTML = `<li class="key"> Rotation </li> <li class="value">${rotation} days </li> `;
 
     let revolution = node[5].textContent;
-    console.log(revolution);
     node[5].innerHTML = `<li class="key"> Revolution </li> <li class="value">${revolution} days </li> `;
     if (j > 2) {
       node[5].innerHTML = `<li class="key"> Revolution </li> <li class="value">${revolution} years </li> `;
     }
 
     let radius = node[6].textContent;
-    console.log(radius);
     node[6].innerHTML = ` <li class="key"> Equatorial Radius </li> <li class="value">${radius} km </li> `;
 
     let image = document.createElement("div");
